dae4357a7f583bdd13b1d0d87cca5149cf4cb61b
JuanMa12 <jumaru-97@hotmail.com>
2017-03-03 14:44:03 -0500

    implement login csss

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,75 @@
-@extends('layouts.app')
-
+@extends('layouts.home')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+    <h1>Sign In And Sign Up Forms</h1>
+    <div class="w3layouts">
+        <div class="signin-agile">
+            <h2>Sign In</h2>
+            @if ($errors->has('email'))
+                <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+            @if ($errors->has('password'))
+                <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
+            @endif
+            <form role="form" method="POST" action="{{ url('/login') }}">
+                {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <input type="email" name="email" class="email" placeholder="Email" value="{{ old('email') }}" required>
                 </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <input type="password" name="password" class="password" placeholder="Password" required>
+                </div>
+                    <ul>
+                        <li>
+                            <input type="checkbox" id="brand1"  name="remember" {{ old('remember') ? 'checked' : ''}}>
+                            <label for="brand1"><span></span>Remember me</label>
+                        </li>
+                    </ul>
+                    <a href="#">Forgot Password?</a><br>
+                    <div class="clear"></div>
+                    <input type="submit" value="Login">
+            </form>
+        </div>
+        <div class="signup-agileinfo">
+            <h3>Sign Up</h3>
+            <p>Phasellus vel urna magna. Quisque molestie felis varius ex faucibus auctor. Aenean sit amet erat ullamcorper,
+                condimentum odio in, commodo tellus.</p>
+            <div class="more">
+                <a class="book popup-with-zoom-anim button-isi zoomIn animated" data-wow-delay=".5s" href="#small-dialog">Sign Up</a>
             </div>
         </div>
+        <div class="clear"></div>
     </div>
-</div>
+    <div class="footer-w3l">
+        <p class="agileinfo"> &copy; 2016 Sign In And Sign Up Forms . All Rights Reserved | Design by
+            <a href="http://w3layouts.com">W3layouts</a></p>
+    </div>
+    <div class="pop-up">
+        <div id="small-dialog" class="mfp-hide book-form">
+            <h3>Sign Up Form </h3>
+            @if ($errors->has('name'))
+                <span class="help-block"><strong>{{ $errors->first('name') }}</strong></span>
+            @endif
+            @if ($errors->has('email'))
+                <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
+            @endif
+            @if ($errors->has('password'))
+                <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
+            @endif
+
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                {{ csrf_field() }}
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <input type="text" name="name" value="{{ old('name') }}" placeholder="Your Name" required/>
+                </div>
+                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <input type="text" name="email" value="{{ old('email') }}" class="email" placeholder="Email" required/>
+                </div>
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <input type="password" name="password" class="password" placeholder="Password" required/>
+                </div>
+                <input type="password" id="password-confirm" name="password_confirmation" class="password" placeholder="Confirm Password" required/>
+                <input type="submit" value="Sign Up">
+            </form>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keywords" content="Allcast, tu creador de canales onlien a bajo precio" />
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('template/css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('template/css/popup-box.css')}}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+        <script type="text/javascript" src="{{asset('template/js/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('template/js/jquery.magnific-popup.js')}}"></script>
+        <script type="text/javascript" src="{{asset('template/js/modernizr.custom.53451.js')}}"></script>
+        <script type="text/javascript" src="{{asset('template/js/popup.js')}}"></script>
+    </head>
+    <body>
+    @yield('content')
+
+
+    </body>
+</html>
