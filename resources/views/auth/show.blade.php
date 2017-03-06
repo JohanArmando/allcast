c4fbbc1bf3c6558c57381ad9a9e8ad76e7531337
Jose Carlos <jcfb90@gmail.com>
2017-03-06 18:21:17 -0500

    go to chanel

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -13,49 +13,16 @@
 
     <div class="w3layouts">
         <div class="signin-agile" style="width: 90%; !important;height: auto !important;">
-            <input type="hidden" name="_method" value="PUT">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <br>
-            <div class="form-group">
                 <label for="title" class="col-sm-3 control-label">Stream title</label>
-                <div class="col-sm-9">
-                    <input type="text" class="form-control" name="title" value="{{$stream->title}}" required="">
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="desc" class="col-sm-3 control-label">Description</label>
-                <div class="col-sm-9">
-                    <textarea rows="4" class="form-control" name="description" style="width: 100%">{{$stream->description}}</textarea>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="domains" class="col-sm-3 control-label">Domain protection</label>
-                <div class="col-sm-9">
-                    <textarea rows="4" class="form-control"  name="domain" style="width: 100%">{{$stream->domain}}</textarea>
-                </div>
-            </div>
-
-            <br>
-            <div class="form-group">
+                <input type="text" class="form-control" name="title" value="{{$stream->title}}" required=""><br>
                 <label for="fms" class="col-sm-3 control-label">Pubish server</label>
-                <div class="col-sm-9">
-                    <input type="text" readonly="" class="form-control" onclick="this.focus();this.select();" id="fms"
-                           value="{{$stream->information->server}}">
-                </div>
-            </div>
-            <div class="form-group">
+                <input type="text" readonly="" class="form-control" onclick="this.focus();this.select();" id="fms" value="{{$stream->information->server}}"><br>
                 <label for="sf" class="col-sm-3 control-label">Stream key</label>
-                <div class="col-sm-9">
-                    <input type="text" readonly="" class="form-control" onclick="this.focus();this.select();" id="sf"
-                           value="{{$stream->information->key}}">
-                </div>
-            </div>
-            <div class="form-group">
+                <input type="text" readonly="" class="form-control" onclick="this.focus();this.select();" id="sf" value="{{$stream->information->key}}"><br>
                 <label for="sf" class="col-sm-3 control-label">Embed code</label>
-                <div class="col-sm-9">
-                    <textarea rows="4" style="width: 100%" id="embed_code" readonly="" class="form-control" onclick="this.focus();this.select();"><script type='text/javascript'>id='{{$stream->id}}'; width='640'; height='360';</script><script type='text/javascript' src='{{url('/player/')}}'></script></textarea>
-                </div>
-            </div>
+                <textarea rows="4" style="width: 100%" id="embed_code" readonly="" class="form-control"><script type='text/javascript'>id='{{$stream->id}}'; width='640'; height='360';</script><script type='text/javascript' src='{{url('/player/')}}'></script></textarea>
+                <a href="{{url('/player')}}"> <input type="submit" value="Go To Stream" style="width:100%;"></a>
         </div>
     </div>
 
