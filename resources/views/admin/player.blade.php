8c41eca81b5abb0374ed96687811c45c6021357d
mdiaz00147 <mdiaz00147@gmail.com>
2017-03-09 09:56:11 -0500

    allcast x

--- resources/views/admin/player.blade.php
+++ resources/views/admin/player.blade.php
@@ -103,7 +103,7 @@
 		<div id="player"></div>
 		<script>
 			var player = new Clappr.Player({
-			source: "http://176.126.83.29/stream/{{$stream}}/playlist.m3u8", 
+			source: "http://origin.allcast.pw:1935/stream/{{$stream}}/playlist.m3u8", 
 			parentId: "#player",
 			width:{{$request->width}},
 			height:{{$request->heigth}},
