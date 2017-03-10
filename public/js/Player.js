0c94705ea011279f54d69fbaf80fd8feab8698f4
mdiaz00147 <mdiaz00147@gmail.com>
2017-03-10 08:10:35 -0500

    test

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,5 +1,4 @@
-$.getScript("/../dashboard/js/jquery-1.9.1.min.js");
-
-$(document).ready(function(){
-	$('#PlayAllCast').append('<iframe class="embed-responsive-item" scrolling="no" src="http://allcast.local/player/'+ id +'/'+ width +'/'+ height +'" width="'+ width +'" height="'+ height +'" frmaeborder=0 ></iframe>')
-});
+window.onload = function() {
+    var player = document.getElementById("PlayAllCast");
+    player.innerHTML = '<iframe class="embed-responsive-item" scrolling="no" src="http://allcast.local/player/'+ id +'/'+ width +'/'+ height +'" width="'+ width +'" height="'+ height +'" frmaeborder=0 ></iframe>';
+}
