ba374ca9786ac261496487d7baec68bea2c28631
mdiaz00147 <mdiaz00147@gmail.com>
2017-03-10 11:43:23 -0500

    fixed embed

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -13,6 +13,7 @@ class PlayerController extends Controller
     {
         $stream     =   Stream::find($request->id);
         $protect_result =	$this->domainProtect($stream);
+
         if ($protect_result == 1) {
         	return view('admin.player', compact('request','stream'));
         }else{
@@ -22,18 +23,23 @@ class PlayerController extends Controller
     public function domainProtect($domain)
     {
     	$domains	=	$domain->domain;
-    	$domains	=	explode(",", $domains);
-    	array_push($domains,'allcast.local','www.allcast.pw', 'allcast.pw' );
-        $domain_referer	=	request()->headers->get('referer');
-        $first_filter	=	explode('//', $domain_referer);
-        $second_filter	=	explode('/', $first_filter[1]);
-        $referer_domain	=	$second_filter[0];
-        if(in_array($referer_domain, $domains))
-    	{
+    	if (!empty($domains)) {
+    		$domains	=	explode(",", $domains);
+	    	array_push($domains,'allcast.local','www.allcast.pw', 'allcast.pw' );
+	        $domain_referer	=	request()->headers->get('referer');
+	        $first_filter	=	explode('//', $domain_referer);
+	        $second_filter	=	explode('/', $first_filter[1]);
+	        $referer_domain	=	$second_filter[0];
+	        if(in_array($referer_domain, $domains))
+	    	{
+	    		return 1;
+	    	}
+	    	else{
+	    		return 0;
+	    	}
+    	}else{
     		return 1;
     	}
-    	else{
-    		return 0;
-    	}
+    	
     }
 }
